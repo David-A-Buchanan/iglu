3cfc8182acf9bedc47ec0178e6a0d8c19ee47610
David Buchanan <davidb235@tamu.edu>
2021-01-20 12:12:44 -0600

    Update docs on cogi

--- R/COGI.R
+++ R/COGI.R
@@ -1,26 +1,24 @@
 #' Calculate percentage below targeted values
 #'
-#' #' @description
-#' The function below_percent produces a tibble object with values equal to
-#' the percentage of glucose measurements below target values. The output columns
-#' correspond to the subject id followed by the target values and the output rows
-#' correspond to the subjects. The values will be between 0 (no measurements)
-#' and 100 (all measurements).
+#' @description
+#' The function cogi produces cogi values in a tibble object.
 #'
 #' @usage
-#' below_percent(data, targets_below = c(50, 80))
+#' cogi(data, targets = c(70, 180))
 #'
 #' @param data DataFrame with column names ("id", "time", and "gl"),
 #' or numeric vector of glucose values.
 #'
-#' @param targets_below Numeric vector of glucose thresholds. Glucose values from
-#' data argument will be compared to each value in the targets_below vector.
-#' Default list is (50, 80).
+#' @param targets Numeric vector of two glucose values for threshold. Glucose values from
+#' data argument will be compared to each value in the targets vector to determine the
+#' time in range and time below range for COGI. The lower value will be used for determining
+#' time below range.
+#' Default list is (70, 180).
 #'
 #' @details
 #' A tibble object with 1 row for each subject, a column for subject id and
 #' column for each target value is returned. NA's will be omitted from the glucose
-#' values in calculation of percent.
+#' values in calculation of cogi.
 #'
 #'
 #' @return If a data.frame object is passed, then a tibble object with
@@ -31,26 +29,31 @@
 #' @export
 #'
 #' @references
-#' Rodbard (2009) Interpretation of continuous glucose monitoring data:
-#' glycemic variability and quality of glycemic control,
-#' \emph{Diabetes Technology and Therapeutics} \strong{11} .55-67,
-#' \doi{10.1089/dia.2008.0132}.
+#' Leelarathna (2020) Evaluating Glucose Control With a Novel Composite
+#' Continuous Glucose Monitoring Index,
+#' \emph{Diabetes Technology and Therapeutics} \strong{14(2)} 277-284,
+#' \doi{10.1177/1932296819838525}.
 #'
 #' @examples
 #'
 #' data(example_data_1_subject)
 #'
-#' below_percent(example_data_1_subject)
-#' below_percent(example_data_1_subject, targets_below = c(50, 100, 180))
+#' cogi(example_data_1_subject)
+#' cogi(example_data_1_subject, targets = c(50, 140))
 #'
 #' data(example_data_5_subject)
 #'
-#' below_percent(example_data_5_subject)
-#' below_percent(example_data_5_subject, targets_below = c(80, 180))
+#' cogi(example_data_5_subject)
+#' cogi(example_data_5_subject, targets = c(80, 180))
 #'
 
 
-COGI <- function(data, targets = c(70,180)){
+cogi <- function(data, targets = c(70,180)){
+  x = target_val = id = NULL
+  rm(list = c("id", "target_val", "x"))
+  data = check_data_columns(data)
+  is_vector = attr(data, "is_vector")
+  targets = sort(as.double(targets))
 
   weight_features = function(feature, scale_range, weight = 1, increasing = TRUE) {
     if (increasing) {
@@ -62,27 +65,22 @@ COGI <- function(data, targets = c(70,180)){
     }
   }
 
-
-
-  x = target_val = id = NULL
-  rm(list = c("id", "target_val", "x"))
-  data = check_data_columns(data)
-  is_vector = attr(data, "is_vector")
-  targets = sort(as.double(targets))
-  ir = in_range_percent(data, list(targets))[,2]
-  br = below_percent(data, targets_below = targets[1])[,2]
-  stddev = sd_glu(data)[,2]
-  subjects = sd_glu(data)[,1]
-
-  w_f = weight_features(ir,c(0,100),weight = .5)+weight_features(br,c(0,15),weight = .35, increasing = FALSE)+weight_features(stddev,c(18,108),weight = .15)
-  out = data.frame(subjects, w_f)
-
   if (is_vector) {
-    out$id = NULL
+    ir = in_range_percent(data, list(targets))[,1]
+    br = below_percent(data, targets_below = targets[1])[,1]
+    stddev = sd_glu(data)$sd
+    w_f = weight_features(ir,c(0,100),weight = .5)+weight_features(br,c(0,15),weight = .35, increasing = FALSE)+weight_features(stddev,c(18,108),weight = .15)
+    out = dplyr::tibble(w_f)
+  } else {
+    ir = in_range_percent(data, list(targets))[,2]
+    br = below_percent(data, targets_below = targets[1])[,2]
+    stddev = sd_glu(data)$sd
+    w_f = weight_features(ir,c(0,100),weight = .5)+weight_features(br,c(0,15),weight = .35, increasing = FALSE)+weight_features(stddev,c(18,108),weight = .15)
+    out = tibble(w_f)
+    out$id = sd_glu(data)$id
+    out = out[,c(2,1)]
+    colnames(out) = c("id", "COGI")
   }
+
   return(out)
 }
-
-
-
-
